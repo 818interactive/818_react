d7ef769bf39514419b16180bb6c1eb46120c5dbd
Petermuturi <pmuturi65@gmail.com>
2017-02-14 20:14:39 +0300

    added children props and router links

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/main/App';
+
+import Home from './components/web/Home';
+import About from './components/web/About';
+import Contact from './components/web/Contact';
+import Faq from './components/web/Faq';
+import Terms from './components/web/Terms';
+import PrivacyPolicy from './components/web/PrivacyPolicy';
 
 ReactDOM.render(
-  <App />,
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			<IndexRoute component={Home}/>
+			<Route path="/about" component={About} />
+			<Route path="/contact" component={Contact} />
+			<Route path="/faq" component={Faq} />
+			<Route path="/terms" component={Terms} />
+			<Route path="/privacy-policy" component={PrivacyPolicy} />
+		</Route>
+	</Router>,
   document.getElementById('interactive_root')
 );
