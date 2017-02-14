6a3130e2f96d89622ffe61d41673c39b829c4827
Petermuturi <pmuturi65@gmail.com>
2017-02-14 21:06:21 +0300

    added struct components

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import Header from '../struct/Header';
+import Footer from '../struct/Footer';
+
+import './app.css'
+
 class App extends Component {
   render() {
     return (
       <div>
-      	{ this.props.children }
+      	<Header/>
+      		{ this.props.children }
+      	<Footer/>
       </div>
     );
   }
