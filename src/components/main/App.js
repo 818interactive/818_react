4c40e2fa60845cba8bda80c5216ce28089d6f1cb
Petermuturi <pmuturi65@gmail.com>
2017-02-16 08:11:14 +0300

    added assets

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
+import { 
+  Link, 
+  Element, 
+  Events, 
+  animateScroll, 
+  scrollSpy } from 'react-scroll';
 
 import Home from '../web/Home';
 import About from '../web/About';
 import Contact from '../web/Contact';
 
+import CurveUp from '../struct/CurveUp';
 import Footer from '../struct/Footer';
 
 import './app.css';
 
 class App extends Component {
+  componentDidMount() {
+    scrollSpy.update();
+  }
+
+  componentWillUnmount(){
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+
+  scrollToTop=()=> {
+    animateScroll.scrollToTop();
+  }
+
   render() {
     return (
       <div>
+
         {/* Home Component */}
-				<Home/>
+        <Element className="element" name="home">
+          <Home/>
+        </Element>
+        <CurveUp/>
         {/* About Component */}
-				<About/>
+        <Element className="element" name="about">
+          <About/>
+        </Element>
+
         {/* Contact Component */}
-				<Contact/>
+        <Element className="element" name="contact">
+				  <Contact/>
+        </Element>
+
         {/* Footer Component */}
       	<Footer/>
+
+        <Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={700}>
+          Go to first  inside container
+        </Link>
+        <Link activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={700}>
+          Go to first  inside container
+        </Link>
+        <Link activeClass="active" to="contact" spy={true} smooth={true} offset={50} duration={700}>
+          Go to first  inside container
+        </Link>
+ 
+        <a onClick={this.scrollToTop}>To the top!</a>
+        <br/>
+        <a onClick={this.scrollToBottom}>To the bottom!</a>
+        <br/>
+        <a onClick={this.scrollTo}>Scroll to 100px from the top</a>
+        <br/>
+        <a onClick={this.scrollMore}>Scroll 100px more from the current position!</a>
       </div>
     );
   }
--- src/components/web/About.js
+++ src/components/web/About.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Grid, Image } from 'semantic-ui-react'
 
 import '../css/about.css';
 
 export default class About extends Component {
 	render() {
 		return (
-			<div id="about">About</div>
+			<div id="about">
+				<p className="about">About</p>
+				<hr style={{width: "50%", marginBottom: "12px"}}/>
+				<Grid>
+					<Grid.Row columns={2}>
+						<Grid.Column>
+							<Image src='/images/design2.jpg' />
+						</Grid.Column>
+						<Grid.Column>
+							<Image src='http://semantic-ui.com/images/wireframe/paragraph.png' />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</div>
 		);
 	}
 }
